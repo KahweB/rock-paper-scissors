ed1983c4ba09c6006eb0573b2a81a4ce6f308a62
Kahve <72060765+KahweB@users.noreply.github.com>
2023-01-12 00:46:45 +0300

    added some comments, deleted unused function

--- script.js
+++ script.js
@@ -1,89 +1,73 @@
-
-/*generates random choice*/
 let playerScore = 0;
 let computerScore = 0;
+
+/*choices arrayindan random index return eder*/
 function getComputerChoice(){ 
     let choices = ["rock","paper","scissors"];
     return choices[Math.floor(Math.random()*3)];
 }
-function playerSelection(choice){
-    return choice
-}
+
 function eventer(){
-    //buttonlarin hepsini buttons degiskenine atar
+    //buttonlarin hepsini list olarak buttons degiskenine atar
     const buttons = document.querySelectorAll(".choice");
-    //butun buttonlara click event listener ekler event callbacki playerselection
-    //functiona atar
+    //butun buttonlara click event listener ekler event callbacki 
+    //playerselection functiona atar
     buttons.forEach(button => {
         button.addEventListener("click", e => {
             let choice = e.target.id;
             playRound(choice,getComputerChoice());
-            
         }); 
     });
 };
+
+//skoru kontrol eder DOM a sonucu yazar return boolean
 function winnerChecker(computerScore,playerScore){
     if(computerScore > playerScore && computerScore == 5){
         document.getElementById("winnerAnnounce").innerHTML = "COMPUTER WON";
         return false;
-        
     }
     else if(playerScore > computerScore && playerScore == 5 ){
         document.getElementById("winnerAnnounce").innerHTML = "PLAYER WON";
         return true;
     }
 }
-/*plays round*/
+
+/*iki secenegi karsilastir score restart*/
 function playRound(playerSelection,computerSelection){
     if(playerSelection === "rock" && computerSelection === "paper"){
-        
         computerScore++;
-        
-        console.log("you choose rock computer choose paper computer got point");
     }
     else if(playerSelection === "rock"&& computerSelection ==="scissors"){
-        
         playerScore++;
-        
-        console.log("you choose rock computer choose scissors player got point");
     }
     else if(playerSelection === "paper"&& computerSelection ==="rock"){
-        
-        playerScore++;
-        
-        console.log("you choose paper computer choose rock player got point");  
+        playerScore++;  
     }
     else if(playerSelection === "paper"&& computerSelection ==="scissors"){
-        
         computerScore++;
-        
-        console.log("you choose paper computer choose scissors computer got point");
     }
     else if(playerSelection === "scissors"&& computerSelection ==="rock"){
-        
         computerScore++;
-        
-        console.log("you choose scissors computer choose rock computer got point");
     }
     else if(playerSelection === "scissors"&& computerSelection ==="paper"){
-        
         playerScore++;
-        
-        console.log("you choose scissors computer choose paper player got point");
     }
     else{
-        console.log("tie");
     }
+    //burasi logic olarak sacma fakat conditionun undefined kalmasi lazim
     let condition = winnerChecker(computerScore,playerScore);
     if (condition == true || condition == false){
         scoreReset();
     }
     htmlScoreUpdater(playerScore,computerScore);
 }
+
+//anlik scoreyi DOM <p> ekler
 function htmlScoreUpdater(playerScore,computerScore){
     document.getElementById("player-ScoreP").innerText = `PLAYER\n${playerScore}`;
     document.getElementById("computer-ScoreP").innerText = `COMPUTER\n${computerScore}`;
 }
+
 function scoreReset(){
     playerScore = 0;
     computerScore = 0;
